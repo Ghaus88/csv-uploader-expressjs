be2fd7fb2a2f0a4c5b1179ff26c1892f5d7097d6
Ali Mohamed Ghaus Bin Abdul Rahim <ghaus@192.168.1.1>
2024-10-03 16:14:44 +0800

    Created data table component.

---
+++ csv-frontend-repo/src/components/table/DataTable.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './dataTable.css';
+import { POST } from '../../types/types';
+interface DataTableProps {
+  data: POST[];
+}
+
+const DataTable: React.FC<DataTableProps> = ({ data }) => {
+  if (data.length === 0) {
+    return <p className="no-data">No data to display</p>;
+  }
+
+  // Extract column headers from the first row of the data
+  const headers = Object.keys(data[0]);
+  console.log('headers', headers);
+  return (
+    <div className="data-table-container">
+      <table className="data-table">
+        <thead>
+          <tr>
+            {headers.map((header) => (
+              <th key={header}>{header}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row) => (
+            <tr key={row.id}>
+              {headers.map((header) => {
+                const validHeader: keyof POST = header;
+                return <td key={validHeader}>{row[validHeader]}</td>;
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default DataTable;
